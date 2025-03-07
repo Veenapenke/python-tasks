e4b766866a56f62a1bc8d1dec23d5eead3b854a2
Veenapenke <171228352+Veenapenke@users.noreply.github.com>
2025-03-07 23:31:23 +0530

    Update access_nested.py

---
+++ Dictonary/access_nested.py
@@ -0,0 +1,7 @@
+students = {
+    101: {"name": "Alice", "age": 20, "grade": "A"},
+    102: {"name": "Bob", "age": 21, "grade": "B"},
+}
+
+print(students[101]["name"]) 
+print(students[102]["age"])
