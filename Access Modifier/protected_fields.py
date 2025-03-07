2f21be9df3a2726c5e2382e6bb1dd1af06512579
Veenapenke <171228352+Veenapenke@users.noreply.github.com>
2025-03-08 00:10:41 +0530

    Update protected_fields.py

---
+++ Access Modifier/protected_fields.py
@@ -0,0 +1,17 @@
+class A:
+    def __init__(self):
+        self._protected_var = "Protected Variable"
+
+    def _protected_method(self):
+        print("This is a protected method")
+
+class B(A):
+    def access_protected(self):
+        print(self._protected_var) 
+        self._protected_method()    
+if __name__ == "__main__":
+    objA = A()
+    print(objA._protected_var)  
+    objA._protected_method()   
+    objB = B()
+    objB.access_protected() 
